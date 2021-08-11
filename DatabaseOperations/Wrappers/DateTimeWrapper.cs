41ff7d06b8bb31db19a812c46799fe13f790c278
MADDY\daeer <daeer73@gmail.com>
2021-08-11 16:39:53 +0100

    Add `DateTimeWrapper` so we can mock out the date time in the backup path
    Added the wrapper and interface.  Created an internal constructor for the `ConnectionOptions`.  Updated the tests to be more robust.

---
+++ DatabaseOperations/Wrappers/DateTimeWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DatabaseOperations.Interfaces;
+
+namespace DatabaseOperations.Wrappers
+{
+    internal class DateTimeWrapper : IDateTimeWrapper
+    {
+        internal DateTimeWrapper()
+        {
+            _dateTime = null;
+        }
+
+        internal DateTimeWrapper(DateTime fixedDateTime)
+        {
+            _dateTime = fixedDateTime;
+        }
+
+        private readonly DateTime? _dateTime;
+
+        public DateTime Now => _dateTime ?? DateTime.Now;
+    }
+}
