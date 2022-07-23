0b4f2b924773ee14eaf417e668de6068b2c8fc01
David Clark <krennic51@gmail.com>
2022-07-23 15:09:45 +0100

    Add connection string properties
    Added the struct to hold the connection string properties.

---
+++ DatabaseOperations/DataTransferObjects/ConnectionProperties.cs
@@ -0,0 +1,14 @@
+﻿namespace DatabaseOperations.DataTransferObjects
+{
+    internal struct ConnectionProperties
+    {
+        internal string ConnectionString { get; set; }
+        internal string ApplicationName { get; set; }
+        internal string DatabaseName { get; set; }
+        internal string ConnectTimeout { get; set; }
+        internal string IntegratedSecurity { get; set; }
+        internal string Password { get; set; }
+        internal string Server { get; set; }
+        internal string UserId { get; set; }
+    }
+}
